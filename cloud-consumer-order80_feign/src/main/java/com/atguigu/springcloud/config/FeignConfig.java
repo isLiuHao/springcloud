07e6c4c61e7a756241579fd68ce502666ba64259
isLiuHao <603400054@qq.com>
2021-01-22 18:00:19 +0800

    springcloud练习

---
+++ cloud-consumer-order80_feign/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud.config;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean//日志
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
