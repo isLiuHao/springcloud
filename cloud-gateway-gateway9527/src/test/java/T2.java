1cfc0bb8caf8dad4878e2ed3a6d411daac725cf2
isLiuHao <603400054@qq.com>
2021-01-29 19:40:12 +0800

    springcloud练习

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime=ZonedDateTime.now();//默认时区
+        System.out.println(zonedDateTime);
+    }
+}
