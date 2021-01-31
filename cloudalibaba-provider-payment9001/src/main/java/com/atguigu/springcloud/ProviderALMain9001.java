a53fc5d1bd40b318b9849944bf0904dca23f3320
isLiuHao <603400054@qq.com>
2021-01-31 22:14:23 +0800

    springcloud练习

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/ProviderALMain9001.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+public class ProviderALMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderALMain9001.class,args);
+    }
+}
